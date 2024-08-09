3619590a937d16720345012e56c814db771d61c9
Vecta <victoronuchukwu16@gmail.com>
2024-08-09 14:30:56 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+class User {
+    email: string
+    name: string
+    city: string = ""
+    constructor(email: string, name: string){
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const vecta = new User("vec@ymail.com", "vickyboy")
+vecta.city = "Imo state"
